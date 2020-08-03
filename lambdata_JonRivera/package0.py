cf283e0f05bdb160f75f313eaf90cb3c0895845a
Jon Rivera <66049971+JonRivera@users.noreply.github.com>
2020-08-03 11:18:44 -0700

    update python package
    update package0

--- lambdata_JonRivera/package0.py
+++ lambdata_JonRivera/package0.py
@@ -1,6 +1,30 @@
-"package0 - a collection of Data Science helpfer functions"
-import pandas as pd 
-import numpy as np
+"Package0 - a collection of Data Science helpfer functions"
 
-ONES= pd.DataFrame(np.ones(10))
-ZEROS= pd.DataFrame(np.zeros(50))
+import pandas as pd 
+import numpy as np 
+import sklearn
+
+from sklearn.model_selection import train_test_split
+def data_null_check(df):
+  """Prints out nulls of dataframe into a nice data frame format"""
+  if type(df)==pd.core.frame.DataFrame:
+    return df.isnull().sum()
+
+def features_select(df,name_target,max_cardinality =20):
+    """creates features, and target from data frame, takes dataframe, 
+    name of feature in strings, and has an optional 3rd parameter of max
+    cardinality allowed"""
+    target = name_target
+    #creating subsets of features, splitted between numerical and categorical
+    train_features =  df.drop(columns = [name_target],axis =1)
+    numeric_features = train_features.select_dtypes(include='number').columns.tolist()
+    cardinality = train_features.select_dtypes(exclude ='number').nunique()
+    categorical_features = cardinality[cardinality <= max_cardinality].index.tolist()
+    #combine lists to extrapolate features of interest
+    features = numeric_features + categorical_features
+    return (features, target)
+
+
+    
+
+
--- setup.py
+++ setup.py
@@ -1,1 +1,33 @@
-"""" lambd_JonRivera - a collection of  Data Science helpter functions """  
+"""" lambda-JonRivera - a collection of  Data Science helpter functions """  
+"""
+lambdata - a collection of Data Science helper functions
+"""
+
+import setuptools
+
+REQUIRED = [
+    "numpy",
+    "pandas",
+    "sklearn"
+]
+
+with open("README.md", "r") as file:
+    LONG_DESCRIPTION = file.read()
+
+setuptools.setup(
+    name="lambdata-JonRivera",
+    version="0.0.0",
+    author="soycode",
+    description="A collection of Data Science helper functions",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JonRivera/Package_Repo",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+    install_requires=REQUIRED,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
