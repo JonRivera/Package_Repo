74115c1116f688ed26aea424c7e5729cdffebe42
Jon Rivera <66049971+JonRivera@users.noreply.github.com>
2020-08-04 14:10:57 -0700

    new version of setup.py

--- lambdata_JonRivera/package0.py
+++ lambdata_JonRivera/package0.py
@@ -1,34 +1,47 @@
-"Package0 - a collection of Data Science helpfer functions"
-
+"""Package0 - a collection of Data Science helpfer functions"""
 import pandas as pd
-import numpy as np
-import sklearn
-from sklearn.model_selection import train_test_split
+
 
 def data_null_check(df):
-  """Prints out nulls of dataframe into a nice data frame format"""
-  if type(df)==pd.core.frame.DataFrame:
-    return df.isnull().sum()
+    """Prints out nulls of dataframe into a nice data frame format"""
+    if type(df) == pd.core.frame.DataFrame:
+        return df.isnull().sum()
 
-def features_select(df,name_target,max_cardinality =20):
-    """creates features, and target from data frame, takes dataframe, 
+
+def features_select(df, name_target, max_cardinality=20):
+    """ creates features, and target from data frame, takes dataframe, 
     name of feature in strings, and has an optional 3rd parameter of max
     cardinality allowed"""
     target = name_target
-    #creating subsets of features, splitted between numerical and categorical
-    train_features =  df.drop(columns = [name_target],axis =1)
+    # creating subsets of features, splitted between numerical and categorical
+    train_features = df.drop(columns=[name_target], axis=1)
     numeric_features = train_features.select_dtypes(include='number').columns.tolist()
-    cardinality = train_features.select_dtypes(exclude ='number').nunique()
+    cardinality = train_features.select_dtypes(exclude='number').nunique()
     categorical_features = cardinality[cardinality <= max_cardinality].index.tolist()
-    #combine lists to extrapolate features of interest
+    # combine lists to extrapolate features of interest
     features = numeric_features + categorical_features
-    return (features, target)
+    return features, target
 
 
-class Game:
-    def __init__(self, player1='Jon',player2='Rivera'):
-        self.rounds=2
-        self.steps =5
-        self.core=4
-        self.player1=player1
-        self.player2=player2
+class Car:
+    """makes a car class"""
+
+    def __init__(self, mpg=40, horsepower=170, price=25000, color='red'):
+        self.mpg = mpg
+        self.horsepower = horsepower
+        self.price = price
+        self.color = color
+
+    def sound(self):
+        return "ROOOM"
+
+
+class Ferrari(Car):
+    """ Uses class enheritance to make Ferrari"""
+
+    def __init__(self, mpg, horsepower, price, color, edition='sport'):
+        super().__init__(mpg, horsepower, price, color)
+        self.edition = edition
+
+    def sound(self):
+        return "boom --VROOM"
